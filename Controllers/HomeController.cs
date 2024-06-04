27eac4bc73ddc7b5af3f39c47eefbb26e1215f50
Mike <michaeldick000@gmail.com>
2024-06-03 22:43:29 -0400

    Added all pages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,17 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Portfolio()
+    public IActionResult Paintings()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Books()
     {
         return View();
     }
